3b824fd67dc01873110d6608b27fbf9332523ea0
Your Name <your.email@example.com>
2025-07-04 13:49:07 +0530

    changed api url in ui for deployment

--- agent-ui/src/App.jsx
+++ agent-ui/src/App.jsx
@@ -4,7 +4,8 @@ import {
 } from "@mui/material";
 import axios from "axios";
 
-const API_URL = "http://localhost:5100";
+// const API_URL = "http://localhost:5100";
+const API_URL = "http://34.47.234.81:5001";
 const AGENT_API_KEY = "5acd6f98c443e42bb1631cb6bcd26dc8a2202f0d3a24e31c5090efa69953e849";
 
 function App() {
